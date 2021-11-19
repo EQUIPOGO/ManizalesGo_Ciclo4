c3b9ac19103fa9a9974bb0ff17429e1c63f4b86c
OskrGutierrez <osagutierrezbo@unal.edu.co>
2021-11-18 21:26:42 -0500

    Implementación cardView y recyclerView

---
+++ app/src/main/java/com/go/manizalesgo/ListaSitiosTuristicosActivity.kt
@@ -0,0 +1,11 @@
+package com.go.manizalesgo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaSitiosTuristicosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_sitios_turisticos)
+    }
+}
---
+++ app/src/main/java/com/go/manizalesgo/SitioTuristico.kt
@@ -0,0 +1,7 @@
+package com.go.manizalesgo
+
+data class SitioTuristico (
+    val nombreST: String,
+    val descripcionST: String,
+    val puntuacion: String
+    )
