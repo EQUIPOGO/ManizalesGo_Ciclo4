295480b959dec97dac9b096259338c55ac16caee
byrondavidd <byronbdmg@gmail.com>
2021-12-11 00:11:38 -0500

    Adelanto

--- manizalezgo/src/main/java/com/chafu/manizalezgo/detail/DetailFragment.kt
+++ manizalezgo/src/main/java/com/chafu/manizalezgo/detail/DetailFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.chafu.manizalezgo.databinding.FragmentDetailBinding
 import com.chafu.manizalezgo.main.MainActivity
@@ -39,6 +40,9 @@ class DetailFragment : Fragment() {
             titulociudadTextView.text = sitioTuristico.nombreST
             descripcionTextView.text = sitioTuristico.descripcionST
             Picasso.get().load(sitioTuristico.urlPicture).into(mainpictureImageView)
+            mapButton.setOnClickListener {
+                findNavController().navigate(DetailFragmentDirections.actionNavigationDetailToMapsFragment())
+            }
         }
 
     }
